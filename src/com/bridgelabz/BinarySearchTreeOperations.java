fc22c705ffe65573ab06a147faa5711112a93904
Surabhi951 <surabhibhagat95@gmail.com>
2022-11-25 21:27:08 +0530

    Added: Created Binary search tree by adding 56 and then adding 30 and 700

---
+++ src/com/bridgelabz/BinarySearchTreeOperations.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class BinarySearchTreeOperations {
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        Node root = null;
+        root = tree.insertData(root, 56);
+        root = tree.insertData(root, 30);
+        root = tree.insertData(root, 70);
+
+        tree.printInorder(root);
+        System.out.println(root);
+    }
+}
